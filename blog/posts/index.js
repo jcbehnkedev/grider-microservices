f88df499dbef4c75b8536fe60855eb1815bf9f5f
Jeff <jcbehnke.dev@gmail.com>
2022-09-14 13:17:22 -0400

    update

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -11,11 +11,11 @@ app.use(cors());
 //store posts we create  
 const posts = {};
 
-app.get('/posts', (req, res) => {
+app.get("/posts", (req, res) => {
     res.send(posts);
 });
 
-app.post('/posts/create', async (req, res) => {
+app.post("/posts/create", async (req, res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
 
@@ -23,8 +23,8 @@ app.post('/posts/create', async (req, res) => {
         id, title
     };
 
-    await axios.post('http://event-bus-srv:4005/events', {
-        type: 'PostCreated',
+    await axios.post("http://event-bus-srv:4005/events", {
+        type: "PostCreated",
         data: {
             id, title
         }
@@ -33,13 +33,13 @@ app.post('/posts/create', async (req, res) => {
     res.status(201).send(posts[id]);
 });
 
-app.post('/events', (req, res) => {
-    console.log('Received Event', req.body.type);
+app.post("/events", (req, res) => {
+    console.log("Received Event", req.body.type);
 
     res.send({});
 });
 
 app.listen(4000, () => {
-    console.log('v55');
-    console.log('Listening on port 4000');
+    console.log("v55");
+    console.log("Listening on port 4000");
 });
