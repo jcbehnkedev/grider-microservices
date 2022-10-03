1d3ea579fb3eced98ff8c46cca4cd4f185930471
Jeff <jcbehnke.dev@gmail.com>
2022-10-03 17:35:37 -0400

    update

--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -9,8 +9,15 @@ interface UserAttrs {
 
 // An interface that describes the properties
 // that a User Model has
-interface UserModel extends mongoose.Model<any> {
-  build(attrs: UserAttrs): any;
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties
+// that a User Document has
+interface UserDoc extends mongoose.Document {
+  email: 'string';
+  password: 'string';
 }
 
 const userSchema = new mongoose.Schema({
@@ -23,15 +30,16 @@ const userSchema = new mongoose.Schema({
     required: true
   }
 });
+
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
-const User = mongoose.model<any, UserModel>('User', userSchema);
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
-User.build({
-  email: 'test@test.com',
-  password: 'pass'
-})
+const user = User.build({
+  email: 'te@df.com',
+  password: 'dfd'
+});
 
 export { User };
