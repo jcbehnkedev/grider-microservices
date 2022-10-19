9b5bc34c96301439fc11be45d017484adbf0908d
Jeff <jcbehnke.dev@gmail.com>
2022-10-19 11:24:05 -0400

    update

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
+import { currentUser } from '../middleware/current-user';
+
 const router = express.Router();
 
-router.get('/api/user/currentUser', (req, res) => {
-  res.send('Hi there!!');
+router.get('/api/users/currentUser', currentUser, (req, res) => {
+  res.send( {currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
