b81ab3ce3f43a50ee67e8c06c10930394d3d4895
Jeff <jcbehnke.dev@gmail.com>
2022-10-25 16:05:32 -0400

    update

---
+++ ticketing/auth/src/routes/__test__/signout.test.ts
@@ -0,0 +1,33 @@
+import request  from 'supertest';
+import { app } from '../../app';
+
+it('clears the cookie after signing out', async() => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test123@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+    const response = await request(app)
+    .post('/api/users/signout')
+    .send({})
+    .expect(200);
+ 
+  //expect(
+  //  response.get('Set-Cookie').shift()
+  //).toMatch(/1970/);
+  //expect(response.get('Set-Cookie')).toBeDefined();
+  expect(response.get('Set-Cookie')[0]).toEqual(
+    'session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly')
+});  
+/*
+  const response = await request(app)
+    .post('/api/users/signout')
+    .send({})
+    .expect(200);
+
+  console.log(response.get('Set-Cookie'));  
+  expect(response.get('Set-Cookie')).toBeDefined();
+*/
