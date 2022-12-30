492291e20c9df70eacad40027d4afa853b715bb0
Jeff <jcbehnke.dev@gmail.com>
2022-12-30 14:06:18 -0500

    update

--- ticketing/nats-test/src/listener.ts
+++ ticketing/nats-test/src/listener.ts
@@ -1,17 +1,41 @@
 import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+import { sortAndDeduplicateDiagnostics } from 'typescript';
 
 console.clear();
 
-const stan = nats.connect('ticketing', '123', {
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'https://localhost:4222',
 });
 
 stan.on('connect', () => {
   console.log('Listener connect to NATS');
 
-  const subscription = stan.subscribe('ticket:created');
+  stan.on('close', () => {
+    console.log('NATS connection closed!');
+    process.exit();
+  });
+
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName('accounting-service');
+
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'queue-group-name',
+    options
+  );
 
   subscription.on('message', (msg: Message) => {
-    console.log('Message received');
+    const data = msg.getData();
+
+    console.log(`Message received #${msg.getSequence()}, with data: ${data}`);
+
+    msg.ack();
   });
 });
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
